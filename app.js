5f523a200449bfd3841f0516c53143aeb70c013c
Rony Lubman <ronyl5@Ronys-MacBook-Pro.local>
2021-09-05 18:46:32 +0300

    Responsiveness
    Fixed home page responsiveness

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use(cookieParser());
 app.use('/', routes);
 
 // database connection
-const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI || `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.0cco4.mongodb.net/site-database?retryWrites=true&w=majority`;
+;
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
  .then(() => console.log('Connected successfully to MongoDB!'))
  .catch(err => console.error('Something went wrong', err));
